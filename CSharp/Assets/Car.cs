441320ed783bea331d9b28f0b7424bb6e23f5bbe
dy956122 <dy956122@gmail.com>
2020-04-08 15:29:44 +0800

    認識欄位

--- CSharp/Assets/Car.cs
+++ CSharp/Assets/Car.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 
-// 註解 用來說明 & 紀錄 ... 只會在此腳本內看到
-// 摘要 Summary : 描述此內容,會在其他腳本內看到,提高維護性及擴充性,請打///
+// 註解 用來說明 、 紀錄 ... 只會在此腳本內看到
+// 摘要 Summary : 描述此內容,會在其他腳本內看到,提高維護性及擴充性(可讀性),請打3個/
+// 新增的C# 放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
 /// 定義汽車物件
 /// </summary>
@@ -12,17 +14,45 @@ public class Car : MonoBehaviour
     // CC數      - 2000         - 整數 int
     // 重量      - 100.9        - 浮點數 float
     // 品牌      - BMW          - 字串(文字) String
-    // 剎車      - 是否開啟      - 布林值 bool - true ` false
+    // 剎車      - 是否開啟      - 布林值 bool - true 、 false
 
     // 定義欄位語法:
-    // 資料類型 欄位名稱(指定 值);
-    // 預設值
-    // int & float 為0
-    // String 為""
-    // bool 為 false
+    // 修飾詞 資料類型 欄位名稱(指定 值);
 
-    int cc = 2000 ;
-    float weight = 100.9f ;
-    string brand = "BMW" ;
-    bool brake = false ; // 也可寫成 bool brake,因為預設值是false
+    // 修飾詞:
+    // 公開 public : 允許其他類別存取,顯示在屬性面板 (Inspector)
+    // 私人 private(預設) : 不允許 、 不顯示
+
+    // 預設值
+    // int 、 float 為 0
+    // String 為 ""
+    // bool 為 false
+    // 欄位資料 Attribute: Unity 提供用於輔助欄位的功能
+    // 語法: [屬性名稱(值)]
+    // 標題 [Header("字串")]
+    // 提示 [Tooltips("字串")]
+    // 範圍 [Range(最小值,最大值] - 僅限於數值類型資料 int、 float
+    
+    /// <summary>
+    /// 汽車的CC數 
+    /// </summary>
+    [Header("汽車的CC數"),Range(500,5000)]
+    public int cc = 2000 ;
+    /// <summary>
+    /// 汽車重量
+    /// </summary>
+    [Tooltip("這個欄位是用來存放汽車的噸位")]
+    [Range(50,350)]
+    public float weight = 100.9f ;
+    /// <summary>
+    /// 品牌
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("代表這台車子的品牌")]
+    public string brand = "BMW" ;
+    /// <summary>
+    /// 是否剎車
+    /// </summary>
+    [Header("是否煞車") ,Tooltip("用來儲存這台車子目前是否為剎車狀態")]
+    public bool brake = false ; // 也可寫成 bool brake; ,因為預設值是false
 }
--- CSharp/Assets/Class1.cs
+++ CSharp/Assets/Class1.cs
@@ -11,6 +11,12 @@ public class Class1 : MonoBehaviour
     // 1.大小不一樣
     // 2.括號成對出現 () [] {} '' "" <>
 
+    [Header("汽車1號")]
+    public Car car1;
+
+    [Header("汽車2號")]
+    public Car car2;
+
     //  事件 : 在特定時間會已指定次數執行的方法 (名稱為藍色)
     // 開始事件: 播放遊戲時執行一次 - 初始設定
     private void Start() // 小寫的start不一樣,要確認Start是否為藍色
@@ -18,6 +24,18 @@ public class Class1 : MonoBehaviour
         // 輸出(文字訊息);
         // C# 結尾為分號 ;
         print("Hello World!!, 哈囉,沃德!!");
+
+        //取得 get ,顯示資料數據
+        print("汽車 1 號的CC數:" + car1.cc);
+        print("汽車 2 號的品牌:" + car2.brand);
+
+
+        //設定 set
+        car1.brake = true ;
+        car1.brand = "TOYOTA" ;
+        car1.cc = 9999 ;
+        car1.weight = 999.9f ;
+        
     }
 
 
