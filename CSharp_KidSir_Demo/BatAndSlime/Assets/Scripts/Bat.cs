7efd555534d06e5a6d9611af1d9854f41f27faaf
dy956122 <dy956122@gmail.com>
2020-05-05 14:27:23 +0800

    上傳老師的蝙蝠與史萊姆
    程式碼很乾淨+_+

---
+++ CSharp_KidSir_Demo/BatAndSlime/Assets/Scripts/Bat.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Bat : MonoBehaviour
+{
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("史萊姆")]
+    public Slime slime;
+
+    private float hp = 150;
+    private float atk = 10;
+    private float cure = 20;
+
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    public void Attack()
+    {
+        // 呼叫 史萊姆 受傷方法(蝙蝠攻擊力)
+        slime.Damage(atk);
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(float damage)
+    {
+        hp -= damage; // hp = hp - damage;
+        message.text =  "<b><color=#293729>" + gameObject.name + " </color></b> - 受到傷害：" + damage;
+        message.text += "\n<b><color=#293729>" + gameObject.name + "</color></b> - 血量剩下：" + hp;
+    }
+
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        hp += cure;
+        message.text = "<b><color=#293729>" + gameObject.name + " </color></b> - 受到治癒：" + cure;
+        message.text += "\n<b><color=#293729>" + gameObject.name + "</color></b> - 血量剩下：" + hp;
+    }
+}
---
+++ CSharp_KidSir_Demo/BatAndSlime/Assets/Scripts/Slime.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Slime : MonoBehaviour
+{
+    [Header("顯示訊息")]
+    public Text message;
+    [Header("蝙蝠")]
+    public Bat bat;
+
+    private float hp = 200;
+    private float atk = 20;
+    private float cure = 10;
+
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    public void Attack()
+    {
+        // 呼叫 蝙蝠 受傷方法(史萊姆攻擊力)
+        bat.Damage(atk);
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(float damage)
+    {
+        hp -= damage; // hp = hp - damage;
+        message.text = "<b><color=#A0AC15>" + gameObject.name + "</color></b> - 受到傷害：" + damage;
+        message.text += "\n<b><color=#A0AC15>" + gameObject.name + "</color></b> - 血量剩下：" + hp;
+    }
+
+    /// <summary>
+    /// 治癒
+    /// </summary>
+    public void Cure()
+    {
+        hp += cure;
+        message.text = "<b><color=#A0AC15>" + gameObject.name + "</color></b> - 受到治癒：" + cure;
+        message.text += "\n<b><color=#A0AC15>" + gameObject.name + "</color></b> - 血量剩下：" + hp;
+    }
+}
